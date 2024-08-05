7d28c0606cf42ad1a98f7ed2fbdfe03a8f81cb29
Alex <ansechin2012@ya.ru>
2024-08-05 14:06:19 +0300

    2

--- Seminar1/Task0.java
+++ Seminar1/Task0.java
@@ -1,3 +1,4 @@
+// Задание №0
 // Реализуйте метод, принимающий в качестве аргумента
 // целочисленный массив.
 // Если длина массива меньше некоторого заданного минимума,
@@ -5,3 +6,24 @@
 // массива.
 
 
+
+package Seminar1;
+
+public class Task0 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int min_length = 1;
+        int result = checkArrayLength(arr, min_length);
+        System.out.println(result);
+    }
+
+
+    public static int checkArrayLength(int[] arr, int min_length) {
+        if (arr.length < min_length){
+            return -1;
+        }
+        else {
+            return arr.length;
+        }
+    }
+}
