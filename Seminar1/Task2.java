2881d00fc5316f1a44940d75dbf8c66ca48eccc6
Alex <ansechin2012@ya.ru>
2024-08-05 14:29:22 +0300

    1

--- Seminar1/Task2.java
+++ Seminar1/Task2.java
@@ -11,13 +11,31 @@ package Seminar1;
 
 public class Task2 {
     public static void main(String[] args) {
-        int[][] matrix = { { 1, 0, 1 }, { 1, 1, 1 }, { 0, 0, 0 } };
+        int[][] matrix = { { 1, 0, 1 }, { 1, 1, 1 }, { 0, 0, 1 } };
+        int sum = sumElements(matrix);
+        System.out.println(sum);
     }
 
     public static int sumElements(int[][] matrix) {
         int n = matrix.length;
-        for (int[] row: matrix) {
-            
+        for (int[] row : matrix) {
+            if (row.length != n) {
+                throw new RuntimeException("Количество строк не равно количеству столбцов!");
+            }
         }
+        for (int[] row : matrix) {
+            for (int element : row) {
+                if (element != 0 && element != 1) {
+                    throw new RuntimeException("Массив должен содержать только 0 или 1!");
+                }
+            }
+        }
+        int sum = 0;
+        for (int[] row : matrix) {
+            for (int element : row) {
+                sum += element;
+            }
+        }
+        return sum;
     }
 }
