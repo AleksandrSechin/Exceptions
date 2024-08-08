30e518f64053c557e82aa5800a1f1a1927e4fd15
Alex <ansechin2012@ya.ru>
2024-08-08 15:17:21 +0300

    seminar2

---
+++ Seminar2/Task1.java
@@ -0,0 +1,25 @@
+package Seminar1.Seminar2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int[] arr = new int[10];
+            System.out.println("Укажите индекс элемента массива, в который хотите записать значение 1");
+            int index = 0;
+            if (scanner.hasNextInt()) {
+                index = scanner.nextInt();
+            } else {
+                System.out.println("Укажите индекс элемента массива - целое число!");
+            }
+            if (index > arr.length) {
+                System.out.println("Указан индекс за пределами массива");
+            } else {
+                arr[index] = 1;
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
